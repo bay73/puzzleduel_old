1c89f3f27f193f2f622d795388b97f31d76c9a4e
Andrey Bogdanov <bay73@mail.ru>
2015-02-07 21:36:29 +0000

    Match historr page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ module.exports = function(app){
    app.get('/', require('./start').get);
    app.get('/sudokuduel', require('./sudoku').get);
    app.get('/rating', require('./rating').get);
+   app.get('/matchhistory', require('./matchhistory').get);
 
    app.post('/info', require('./info').post);
-}
+};
