d7307fb0a6e2f419d4dc4f923229861873ba4b31
Andrey Bogdanov <bay73@mail.ru>
2015-02-07 18:12:10 +0000

    Rating page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 module.exports = function(app){
    app.get('/', require('./start').get);
    app.get('/sudokuduel', require('./sudoku').get);
+   app.get('/rating', require('./rating').get);
 
    app.post('/info', require('./info').post);
 }
--- routes/rating.js
+++ routes/rating.js
@@ -4,10 +4,9 @@ exports.get = function(req, res, next){
     AllUser
     .find({type: {$ne: 'anonym'}})
     .sort({rating: -1})
-    .limit(14)
     .select('id displayName rating')
     .exec(function(err, data){
         if(err) return next(err);
-        res.render('index', { title: 'PuzzleDuel', languages: require('../translation').languages(), ratings: data });
+        res.render('rating', { title: 'PuzzleDuel', languages: require('../translation').languages(), ratings: data });
     });
 };
