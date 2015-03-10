311b562afb1020b7e08b9eef9662d08eef2afccc
Andrey Bogdanov <bay73@mail.ru>
2015-03-10 10:46:10 +0000

    Fix Coockie error

--- scripts/socket.js
+++ scripts/socket.js
@@ -24,8 +24,17 @@ module.exports = function(server) {
         async.waterfall([
             function(callback) {
                 var cookies = cookie.parse(handshake.headers.cookie || '');
+                if(!cookies) {
+                    return callback(null);
+                }
                 var sidCookie = cookies[config.get('session:name')];
+                if(!sidCookie) {
+                    return callback(null);
+                }
                 var sid = cookieParser.signedCookie(sidCookie, config.get('session:secret'));
+                if(!sid) {
+                    return callback(null);
+                }
                 loadSession(sid, callback);
             },
             function(session, callback) {
