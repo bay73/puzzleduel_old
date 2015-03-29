00304e9cd19e3757a501fa098877fc8d5695f7d0
Andrey Bogdanov <bay73@mail.ru>
2015-03-29 14:51:00 +0000

    Debug

--- routes/invitations.js
+++ routes/invitations.js
@@ -216,7 +216,7 @@ var change = function(req, res, next){
     }
     newData.startTime = calltimefrom ? buildDateTime(invitation.startTime, calltimefrom) : invitation.startTime;
     newData.endTime = calltimeto ? buildDateTime(invitation.startTime, calltimeto) : invitation.endTime;
-    console.log('found!!!);
+    console.log('found!!!');
     if(invitation.user.equals(req.user._id)){
       return changeOut(invitation, newData, req, res, next);
     } else if (invitation.opponent == null){
